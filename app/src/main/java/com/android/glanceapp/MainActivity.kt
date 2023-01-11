8f883b4d61be9fa185d322d884842fcc22c1cf0a
YudizYagnik <yagnik.v@yudiz.com>
2023-01-11 17:45:16 +0530

    simple widget worked

--- app/src/main/java/com/android/glanceapp/MainActivity.kt
+++ app/src/main/java/com/android/glanceapp/MainActivity.kt
@@ -1,58 +1,35 @@
 package com.android.glanceapp
 
-import android.appwidget.AppWidgetHost
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Done
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.currentCompositionLocalContext
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.DpSize
-import androidx.compose.ui.unit.dp
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
+import androidx.glance.text.Text
 import com.android.glanceapp.ui.theme.GlanceAppTheme
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             GlanceAppTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    GreetingsWidget("Android")
-                }
+
             }
         }
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    GlanceAppTheme {
-        GreetingsWidget("Android")
-    }
+class GlanceWidgetProvider : GlanceAppWidgetReceiver() {
+    override val glanceAppWidget: GlanceAppWidget
+        get() = WidgetItems()
 }
 
-class GreetingsWidget(private val name: String): GlanceAppWidget() {
+class WidgetItems : GlanceAppWidget() {
     @Composable
     override fun Content() {
-        Text(text = "Hello $name")
+        Text(
+            text = "Hello Android"
+        )
     }
-}
-
-class GreetingsWidgetReceiver : GlanceAppWidgetReceiver() {
-
-    override val glanceAppWidget = GreetingsWidget("Glance")
 }
